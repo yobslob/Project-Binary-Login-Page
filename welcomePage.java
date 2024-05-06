0a3817c39aa603b3b433cfe25e2550c3eba0ce88
Yogesh <yogeshxiix@gmail.com>
2024-05-06 22:04:07 +0530

    Update welcomePage.java

--- welcomePage.java
+++ welcomePage.java
@@ -1,5 +1,23 @@
-package Login;
+package secondProgram;
+import java.awt.Font;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class welcomePage {
-    
+	
+		JFrame frame = new JFrame();
+		JLabel welcomeLabel = new JLabel("Welcome to Instagram!");
+		
+		welcomePage(String userId){
+			
+			welcomeLabel.setBounds(0,0,200,30);
+			welcomeLabel.setFont(new Font(null,Font.PLAIN,16));
+			welcomeLabel.setText("Hello "+userId);
+			frame.add(welcomeLabel);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(400,400);
+			frame.setLayout(null);
+			frame.setVisible(true);
+		}
+		
 }
