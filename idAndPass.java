0d6bc25cb0e505d67fbcedc8893a2d5e47c5cb62
Yogesh <yogeshxiix@gmail.com>
2024-05-06 22:02:08 +0530

    Update idAndPass.java

--- idAndPass.java
+++ idAndPass.java
@@ -1,18 +1,21 @@
-package Login;
+package secondProgram;
 import java.util.HashMap;
 
 public class idAndPass {
-    
-    HashMap<String,String> loginInfo = new HashMap<String,String>();
 
-    idAndPass(){
+		private HashMap<String,String> loginInfo = new HashMap<String,String>();
 
-        loginInfo.put("Ramesh","Ramesh@123");
-        loginInfo.put("Chameli","Chameli@123");
-        loginInfo.put("Gulabo","Gulabo@123");
-    }
-
-    protected HashMap<String, String> getLoginInfo() {
-        return loginInfo;
-    }
+		public idAndPass(){
+			
+			loginInfo.put("Mukesh","Muk@123");
+			loginInfo.put("Binod","Bin@123");
+			loginInfo.put("Rani","Ran@123");
+		}
+		
+		protected HashMap getLoginInfo() {
+			return loginInfo;
+		}
+		
+		
+		
 }
